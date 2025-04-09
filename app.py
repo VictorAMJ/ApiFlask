ef213d60e3587767291d4f35a57070689ad25f8c
Yara Castro <yara.lima@aluno.faculdadeimpacta.com.br>
2025-04-09 17:45:51 -0300

    modificações no app.py e adição dos testes TDD

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 from config import app
-#from '''professor'''import rota 
-#from '''aluno'' import rota
-from turma.routes import turma
+from professor.routesProfessor import professor
+from aluno.routesAluno import aluno
+from turma.routesTurma import turma
 
-#app.register_blueprint(professor)
-#app.register_blueprint(aluno)
+app.register_blueprint(professor)
+app.register_blueprint(aluno)
 app.register_blueprint(turma)
 
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], port= app.config['PORT'], debug=app.config['DEBUG'])
-
